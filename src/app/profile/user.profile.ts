a8ba359e1fdbe9b36a99e27ef4b8016f6b9895fa
Jugooch <justicegooch@hotmail.com>
2023-02-28 18:50:08 -0700

    This commit adds the base navbar to navigate the app

---
+++ src/app/profile/user.profile.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-userprofile',
+  templateUrl: './user.profile.html',
+  styleUrls: ['./user.profile.scss'],
+})
+export class UserProfile implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
